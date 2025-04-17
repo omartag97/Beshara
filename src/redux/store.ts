a892c64a5276567a6bbc9608703e108e089bf3a3
otag.c <omar.tag@wscdev.net>
2025-04-17 16:14:14 +0200

    refactor: simplify store configuration and enhance type definitions

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,66 +1,67 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-
+import {
+  combineReducers,
+  configureStore,
+  Middleware,
+  Reducer,
+  Slice,
+  Store,
+} from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-
 import { productsAPI } from "./services/products";
 import { cartSlice } from "./slices/cart";
 import { authSlice } from "./slices/auth";
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+interface CustomStore extends Store {
+  asyncReducers: Record<string, Reducer>;
+}
 
 const apis = [productsAPI];
-
 const slices = [cartSlice, authSlice];
 
-export const store = configureStore({
-  reducer: {
+const makeStore = () => {
+  const initialReducers = {
     ...apis.reduce(
       (acc, api) => ({ ...acc, [api.reducerPath]: api.reducer }),
-      {},
+      {} as Record<string, Reducer>,
     ),
     ...slices.reduce(
       (acc, slice) => ({ ...acc, [slice.name]: slice.reducer }),
-      {},
+      {} as Record<string, Reducer>,
     ),
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apis.map((api) => api.middleware)),
-});
+  };
 
-store.asyncReducers = {};
+  const store = configureStore({
+    reducer: initialReducers,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: ["persist/PERSIST"],
+        },
+      }).concat(productsAPI.middleware as Middleware),
+  }) as CustomStore;
 
-apis.forEach((api) => {
-  store.asyncReducers[api.reducerPath] = api.reducer;
-});
+  store.asyncReducers = initialReducers;
+  return store;
+};
 
-slices.forEach((slice) => {
-  store.asyncReducers[slice.name] = slice.reducer;
-});
+export const store = makeStore();
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 const createReducerInjector = () => {
-  const injectReducer = (key, asyncSlice) => {
-    slices.push(asyncSlice);
+  const injectReducer = <T>(key: string, asyncSlice: Slice<T>) => {
     store.asyncReducers[key] = asyncSlice.reducer;
-    store.replaceReducer(
-      combineReducers({
-        ...store.asyncReducers,
-      }),
-    );
+    store.replaceReducer(combineReducers(store.asyncReducers));
   };
 
   return injectReducer;
 };
 
 const createAPIInjector = () => {
-  const injectAPI = (key, asyncAPI) => {
-    apis.push(asyncAPI);
+  const injectAPI = (key: string, asyncAPI: typeof productsAPI) => {
     store.asyncReducers[key] = asyncAPI.reducer;
-    store.replaceReducer(
-      combineReducers({
-        ...store.asyncReducers,
-      }),
-    );
+    store.replaceReducer(combineReducers(store.asyncReducers));
   };
 
   return injectAPI;
